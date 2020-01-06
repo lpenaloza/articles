f769789dc02a797251c2bc2e4802d039d8cb4d0a
Luis A Peñaloza Guette <ing.luispenalozag@gmail.com>
2020-01-06 10:05:26 -0500

    Cambio de conexión

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+/*mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('La conexión a la base de datos se ha realizado bien!!!');
 
@@ -16,4 +16,17 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
             console.log('Server corriendo em http://localhost:' + port)
         });
 
-    });
+});*/
+
+// Connect DB
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(db => console.log('DB Connected'))
+    .catch(err => console.log(err))
+app.listen(port, () => {
+    console.log(`app listening on port ${port}!`)
+})
