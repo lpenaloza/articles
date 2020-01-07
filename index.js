24d2ace00290d8b139c732b89cc7de883cfebf60
Luis A Peñaloza Guette <ing.luispenalozag@gmail.com>
2020-01-07 18:27:39 -0500

    Imagenes

--- index.js
+++ index.js
@@ -3,21 +3,10 @@ require('dotenv').config();
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-/*mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('La conexión a la base de datos se ha realizado bien!!!');
-
-        // Crear servidor y ponerme a escuchar peticiones HTTP
-        app.listen(port, () => {
-            console.log('Server corriendo em http://localhost:' + port)
-        });
-
-});*/
-
 // Connect DB
 mongoose.connect(
     process.env.MONGODB_URI,
